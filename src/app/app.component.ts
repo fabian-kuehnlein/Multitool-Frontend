180dfdd4f0cf2b94b428b58849a1fc67a9efd991
Fabian Kühnlein <fabian.kuehnlein@outlook.com>
2026-07-10 21:12:01 +0200

    feat(theme): add dark mode and enable switching between light and dark themes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy, inject } from '@angular/core';
 import { MatIconRegistry } from '@angular/material/icon';
 import { RouterOutlet } from '@angular/router';
+import { ThemeService } from './core/services/theme.service';
 
 @Component({
     selector: 'app-root',
@@ -10,7 +11,9 @@ import { RouterOutlet } from '@angular/router';
     styleUrl: './app.component.scss'
 })
 export class AppComponent implements OnInit {
-    constructor(private matIconReg: MatIconRegistry) {}
+    private readonly matIconReg = inject(MatIconRegistry);
+    // keep in AppComponent to ensure the ThemeService is instantiated at the root level
+    private readonly themeService = inject(ThemeService);
 
     ngOnInit(): void {
         this.matIconReg.setDefaultFontSetClass('material-symbols-outlined');
