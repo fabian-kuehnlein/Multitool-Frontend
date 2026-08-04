a8e734738327f0388b545dc5dc3182dd75042f50
Fabian Kühnlein <Fabian.kuehnlein@outlook.com>
2026-08-04 19:43:25 +0200

    refactor(todo): apply coding conventions and split tool into smaller modules

---
+++ src/app/modules/todo/pages/components/todo-dialog/todo-dialog-form.service.ts
@@ -0,0 +1,50 @@
+import { Injectable, inject } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import dayjs from 'dayjs';
+import {
+    CreateTodoDto,
+    Priority,
+    Todo,
+    UpdateTodoDto,
+} from '../../../models/todo.model';
+
+@Injectable()
+export class TodoDialogFormService {
+    private readonly fb = inject(FormBuilder);
+
+    readonly form = this.fb.nonNullable.group({
+        title: ['', [Validators.required]],
+        description: [''],
+        priority: [Priority.Medium, [Validators.required]],
+        dueDate: this.fb.control<string | null>(null),
+        categoryId: ['', [Validators.required]],
+    });
+
+    patchFrom(todo: Todo): void {
+        this.form.patchValue({
+            title: todo.title,
+            description: todo.description ?? '',
+            priority: todo.priority,
+            dueDate: todo.dueDate ?? null,
+            categoryId: todo.categoryId,
+        });
+    }
+
+    getCreateData(): CreateTodoDto {
+        const value = this.form.getRawValue();
+        return {
+            title: value.title,
+            description:
+                value.description?.trim() === '' ? null : value.description,
+            categoryId: value.categoryId,
+            priority: value.priority,
+            dueDate: value.dueDate
+                ? dayjs(value.dueDate).format('YYYY-MM-DDTHH:mm:ss')
+                : null,
+        };
+    }
+
+    getUpdateData(isDone: boolean): UpdateTodoDto {
+        return { ...this.getCreateData(), isDone };
+    }
+}
---
+++ src/app/modules/todo/pages/components/todo-item/todo-item.component.ts
@@ -0,0 +1,79 @@
+import {
+    Component,
+    EventEmitter,
+    inject,
+    Input,
+    Output,
+    signal,
+    ChangeDetectionStrategy,
+} from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { UI_MODULES } from '../../../../../shared/utilities/material-ui';
+import { Todo } from '../../../models/todo.model';
+import { Category } from '../../../../../shared/models/category.model';
+import { CategoryService } from '../../../../../shared/services/category.service';
+import { getPriorityColor } from '../../../utilities/todo.config';
+
+@Component({
+    selector: 'app-todo-item',
+    standalone: true,
+    imports: [CommonModule, UI_MODULES],
+    templateUrl: './todo-item.component.html',
+    styleUrl: './todo-item.component.scss',
+    changeDetection: ChangeDetectionStrategy.Eager,
+})
+export class TodoItemComponent {
+    protected readonly getPriorityColor = getPriorityColor;
+
+    @Input({ required: true }) todo!: Todo;
+    @Input() mobile = false;
+
+    @Output() toggleDone = new EventEmitter<Todo>();
+    @Output() edit = new EventEmitter<Todo>();
+    @Output() delete = new EventEmitter<Todo>();
+    @Output() goToDate = new EventEmitter<string>();
+
+    protected readonly expanded = signal(false);
+
+    private readonly categoryService = inject(CategoryService);
+
+    toggleExpand(): void {
+        this.expanded.update((value) => !value);
+    }
+
+    onItemClick(): void {
+        if (this.mobile) {
+            this.toggleExpand();
+        }
+    }
+
+    onCheckboxClick(event: Event): void {
+        if (this.mobile) {
+            event.stopPropagation();
+        }
+    }
+
+    onToggleDone(): void {
+        this.toggleDone.emit(this.todo);
+    }
+
+    onEdit(): void {
+        this.edit.emit(this.todo);
+    }
+
+    onDelete(): void {
+        this.delete.emit(this.todo);
+    }
+
+    onGoToDate(): void {
+        if (this.todo.dueDate) {
+            this.goToDate.emit(this.todo.dueDate);
+        }
+    }
+
+    getCategory(categoryId: string): Category | undefined {
+        return this.categoryService
+            .categories()
+            .find((category) => category.id === categoryId);
+    }
+}
